de92f17da8ccae2b62963c58fa5b1124ddb138a6
SakaiSyunya <j6f5cgsy@i.softbank.jp>
2020-11-14 16:56:24 +0900

    コメント部分修正1

--- JyankenApp_iOS/JyankenApp/ViewController.swift
+++ JyankenApp_iOS/JyankenApp/ViewController.swift
@@ -11,52 +11,14 @@ class ViewController: UIViewController {
     var youhand: Hand!
     var timer: Timer!
     var dispImageNo = 0
+    var count = 0
+    var type = 0
     
     let enemyHand = [
     "rock",
     "scissors",
     "paper",
     ]
-    
-    @IBAction func rockButton(_ sender: Any) {
-        jyankenPlay(you: .rock)
-    }
-    
-    @IBAction func scissorsButton(_ sender: Any) {
-        jyankenPlay(you: .scissors)
-    }
-    
-    @IBAction func paperButton(_ sender: Any) {
-        jyankenPlay(you: .paper)
-    }
-    
-    func displayImage() {
-        
-        if dispImageNo < 0 {
-            dispImageNo = 2
-        }
-        
-        if dispImageNo > 2 {
-            dispImageNo = 0
-        }
-        
-        let hand = enemyHand[dispImageNo]
-        let image = UIImage(named: hand)
-
-        enemy.image = image
-    }
-    
-    override func viewDidLoad(){
-        
-        super.viewDidLoad()
-        
-        timer = Timer.scheduledTimer(
-            timeInterval: 0.5,
-            target: self,
-            selector:#selector(onTimer),
-            userInfo: nil,
-            repeats: true)
-    }
     
     enum Hand: Int{
         case rock = 0
@@ -70,24 +32,80 @@ class ViewController: UIViewController {
         case lose
     }
     
-    func jyankenPlay(you: Hand){
+    override func viewDidLoad(){
+        
+        super.viewDidLoad()
+    }
+    
+    @IBAction func rockButton(_ sender: Any) {
+        timerStart()
+        type = 0
+    }
+    
+    @IBAction func scissorsButton(_ sender: Any) {
+        timerStart()
+        type = 1
+    }
+    
+    @IBAction func paperButton(_ sender: Any) {
+        timerStart()
+        type = 2
+    }
+    
+    func timerStart(){
+        timer = Timer.scheduledTimer(
+        timeInterval: 0.3,
+        target: self,
+        selector:#selector(onTimer),
+        userInfo: nil,
+        repeats: true)
+    }
+    
+    func displayImage() {
+        
+        if timer != nil && count == 4 {
+            count = 0
+            timer.invalidate()
+            
+            if(type == 0){
+                jankenPlay(you: .rock)
+            
+            }else if(type == 1){
+                jankenPlay(you: .scissors)
+            
+            }else if(type == 2){
+                jankenPlay(you: .paper)
+            }
+            
+        }else{
+        
+            if dispImageNo < 0 {
+                dispImageNo = 2
+            }
+        
+            if dispImageNo > 2 {
+                dispImageNo = 0
+            }
+        
+            let image = UIImage(named: enemyHand[dispImageNo])
+
+            enemy.image = image
+        }
+    }
+    
+    func jankenPlay(you: Hand){
         
         let cp = Int.random(in: 0..<3)
         let ehand = UIImage(named: enemyHand[cp])
         let game = (you.rawValue, cp)
         var ans: Result
         
-        
-        if timer != nil{
-            timer.invalidate()
-        }
-        
         print(cp)
         enemy.image = ehand
         
         switch game {
-            case (let handid, let cphand) where handid == cphand: ans = Result.draw
-            case (let handid, let cphand) where (handid+1) % 3 == cp: ans = Result.win
+            case (let handId, let cpHand) where handId == cpHand: ans = Result.draw
+            case (let handId, let cpHand) where (handId+1) % 3 == cpHand: ans = Result.win
             default:ans = Result.lose
         }
         
@@ -101,6 +119,7 @@ class ViewController: UIViewController {
     @objc func onTimer() {
         
         dispImageNo += 1
+        count += 1
         displayImage()
     }
 }
