08fa768f00ef2c9874c07d4304cf66cb8d6fc926
SakaiSyunya <j6f5cgsy@i.softbank.jp>
2021-02-08 17:49:38 +0900

    CustomView作成

---
+++ Delegate_App/CustomView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomView.swift
+//  Delegate_App
+//
+//  Created by Sakai Syunya on 2021/02/08.
+//  Copyright © 2021 Sakai Syunya. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Delegate_App/Delegate_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Delegate_App
+//
+//  Created by Sakai Syunya on 2021/02/08.
+//  Copyright © 2021 Sakai Syunya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
