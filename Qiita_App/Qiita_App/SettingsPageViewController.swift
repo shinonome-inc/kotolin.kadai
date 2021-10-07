5fd8b6c53ccaf9489dbbde2e8dec0b8eee69f04a
SakaiSyunya <j6f5cgsy@i.softbank.jp>
2021-10-07 13:45:22 +0900

    staticCellへ移行中

--- Qiita_App/Qiita_App/SettingsPageViewController.swift
+++ Qiita_App/Qiita_App/SettingsPageViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class SettingsPageViewController: UIViewController {
+class SettingsPageViewController: UITableViewController {
     
-    @IBOutlet var appInfoList: UITableView!
-    @IBOutlet var otherList: UITableView!
+    
+    //    @IBOutlet var appInfoList: UITableView!
+//    @IBOutlet var otherList: UITableView!
     
     enum tableViewType: CaseIterable {
         case appInfoCell
@@ -68,12 +69,12 @@ class SettingsPageViewController: UIViewController {
         }
     }
     
-    var cellTitle: [String] {
-        return tableViewInfo.cellTitle
-    }
-    var cellIdentifier: String {
-        return tableViewInfo.cellIdentifier
-    }
+//    var cellTitle: [String] {
+//        return tableViewInfo.cellTitle
+//    }
+//    var cellIdentifier: String {
+//        return tableViewInfo.cellIdentifier
+//    }
     var transitionIdentifier: String {
         return transitionInfo.transionIdentifier
     }
@@ -82,81 +83,107 @@ class SettingsPageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        appInfoList.delegate = self
-        appInfoList.dataSource = self
-        otherList.delegate = self
-        otherList.dataSource = self
     }
 }
 
-extension SettingsPageViewController: UITableViewDataSource {
+//extension SettingsPageViewController: UITableViewDataSource {
+//
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//      // セクションの数を返します
+//      return 2
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        switch section {
+//        case 0: // 「設定」のセクション
+//          return 3
+//        case 1: // 「その他」のセクション
+//          return 1
+//        default: // ここが実行されることはないはず
+//          return 0
+//        }
+////        tableViewInfo = tableViewType.allCases[tableView.tag - 1]
+////        return tableViewInfo.numCells
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        tableViewInfo = tableViewType.allCases[tableView.tag - 1]
+//
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "settingsCell", for: indexPath) as? SettingsPageCellViewController else {
+//            return UITableViewCell()
+//        }
+////
+////        // アプリバージョンのセルのみ選択不可
+////        if cellIdentifier == "AppInfoCell" && indexPath.row == 2 {
+////            cell.selectionStyle = .none
+////        }
+////
+////        cell.setSettingsCell(title: cellTitle[indexPath.row], tableViewType: tableViewInfo)
+//
+//        return cell
+//    }
+//
+////    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+////        tableViewInfo = tableViewType.allCases[tableView.tag - 1]
+////
+////        tableView.num
+////        appInfo = tableView.numberOfRows(inSection: 0)
+////
+////        switch tableViewInfo {
+////        case .appInfoCell:
+////            if indexPath.row == 2 {
+////                return nil
+////            }
+////            return indexPath
+////        case .otherCell:
+////            return indexPath
+////        }
+////    }
+////
+////    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+////        tableViewInfo = tableViewType.allCases[tableView.tag - 1]
+////
+////        return tableViewInfo.cellHeader
+////    }
+//}
+//
+//extension SettingsPageViewController: UITableViewDelegate {
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+////        tableViewInfo = tableViewType.allCases[tableView.tag - 1]
+//        transitionInfo = transitionType.allCases[indexPath.row]
+//        switch indexPath.row {
+//        case 3:
+//            guard let nextVC = storyboard?.instantiateViewController(identifier: "TopPage") else { return }
+//            nextVC.modalPresentationStyle = .fullScreen
+//            AccessTokenDerivery.shared.deleteAccessToken()
+//            tableView.deselectRow(at: indexPath, animated: true)
+//
+//            self.present(nextVC, animated: true, completion: nil)
+//        default:
+//            guard let nextVC = storyboard?.instantiateViewController(identifier: transitionIdentifier) else { return }
+//
+//            tableView.deselectRow(at: indexPath, animated: true)
+//
+//            self.present(nextVC, animated: true, completion: nil)
+//
+//        }
+////        switch tableViewInfo {
+////        case .appInfoCell:
+////            guard let nextVC = storyboard?.instantiateViewController(identifier: transitionIdentifier) else { return }
+////
+////            tableView.deselectRow(at: indexPath, animated: true)
+////
+////            self.present(nextVC, animated: true, completion: nil)
+////
+////        case .otherCell:
+//            guard let nextVC = storyboard?.instantiateViewController(identifier: "TopPage") else { return }
+//            nextVC.modalPresentationStyle = .fullScreen
+//            AccessTokenDerivery.shared.deleteAccessToken()
+//            tableView.deselectRow(at: indexPath, animated: true)
+//
+//            self.present(nextVC, animated: true, completion: nil)
+//       }
+//    }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        tableViewInfo = tableViewType.allCases[tableView.tag - 1]
-        return tableViewInfo.numCells
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        tableViewInfo = tableViewType.allCases[tableView.tag - 1]
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? SettingsPageCellViewController else {
-            return UITableViewCell()
-        }
-        
-        // アプリバージョンのセルのみ選択不可
-        if cellIdentifier == "AppInfoCell" && indexPath.row == 2 {
-            cell.selectionStyle = .none
-        }
-        
-        cell.setSettingsCell(title: cellTitle[indexPath.row], tableViewType: tableViewInfo)
-
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-        tableViewInfo = tableViewType.allCases[tableView.tag - 1]
-        
-        switch tableViewInfo {
-        case .appInfoCell:
-            if indexPath.row == 2 {
-                return nil
-            }
-            return indexPath
-        case .otherCell:
-            return indexPath
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        tableViewInfo = tableViewType.allCases[tableView.tag - 1]
-        
-        return tableViewInfo.cellHeader
-    }
-}
-
-extension SettingsPageViewController: UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableViewInfo = tableViewType.allCases[tableView.tag - 1]
-        transitionInfo = transitionType.allCases[indexPath.row]
-        
-        switch tableViewInfo {
-        case .appInfoCell:
-            guard let nextVC = storyboard?.instantiateViewController(identifier: transitionIdentifier) else { return }
-            
-            tableView.deselectRow(at: indexPath, animated: true)
-            
-            self.present(nextVC, animated: true, completion: nil)
-            
-        case .otherCell:
-            guard let nextVC = storyboard?.instantiateViewController(identifier: "TopPage") else { return }
-            nextVC.modalPresentationStyle = .fullScreen
-            AccessTokenDerivery.shared.deleteAccessToken()
-            tableView.deselectRow(at: indexPath, animated: true)
-            
-            self.present(nextVC, animated: true, completion: nil)
-        }
-    }
-    
-}
+//}
